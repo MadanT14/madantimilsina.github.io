c8b28383cd11906ef25f8126aea9eba57e74684c
Madan Timilsina <madantimilsina43@gmail.com>
2022-10-06 18:32:19 +0545

    updated

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+const clickme = document.querySelector(".hamburger");
+const head = document.querySelector(".navbar");
+const togglenavbar = () => {
+    head.classList.toggle("active");
+}
+
+clickme.addEventListener("click", () => togglenavbar());
+
+// -------------------faded start-----------------------
+
+
